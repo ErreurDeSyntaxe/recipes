d3b7e64ae1a306d6138418e4c43d33d6a51faa2e
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-06-24 11:41:04 +0800

    Add header & footer + styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>
+        Website based off{' '}
+        <a
+          className="link"
+          target="_blank"
+          href="https://www.youtube.com/watch?v=tv6W0Nv5ev0"
+        >
+          this{' '}
+        </a>
+        YouTube video. Images produced with MidJourney version 4.
+      </span>{' '}
+      Visit an{' '}
+      <a className="link" href="./versions/vanilla.html">
+        earlier version
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import marcusImg from './assets/marcus-aurelius.jpg';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <h1 className={styles.headerTitle}>The Best Man to Ever Live</h1>
+
+      <img
+        src={marcusImg}
+        alt="MidJourney representation of Marcus Aurelius, Emperor Philosopher"
+        className={styles.headerImg}
+      />
+
+      <blockquote className={styles.headerQuotationContainer}>
+        <p className={styles.headerQuotation}>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stoicism takes the position that
+          the wise, the good man, the philosopher is a man who lives in
+          accordance with nature. He fears only abdicating his moral
+          responsibility. He is not afraid of pain. He is not afraid of death.
+          He is not afraid of poverty. He is not afraid of any of the
+          vicissitudes of the Human Condition. He fears only that he should let
+          himself down and that he should be less than a complete human being.
+        </p>
+
+        <footer className={styles.headerQuotationAuthor}>
+          &mdash; Dr Michael Sugrue{' '}
+          <a
+            className="link"
+            href="https://www.youtube.com/watch?v=Auuk1y4DRgk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            (source)
+          </a>
+        </footer>
+      </blockquote>
+    </header>
+  );
+}
+
+export default Header;
