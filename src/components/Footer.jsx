1aad30696e6ddb89cb8313cd99c500ba1e4b9d36
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-11-26 15:01:33 +0800

    Remove previous version link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,14 @@ function Footer() {
       <span>
         Website based off{' '}
         <a
-          className="link"
-          target="_blank"
-          href="https://www.youtube.com/watch?v=tv6W0Nv5ev0"
+          className='link'
+          target='_blank'
+          href='https://www.youtube.com/watch?v=tv6W0Nv5ev0'
         >
-          this{' '}
+          {''}this YouTube video{''}
         </a>
-        YouTube video. Images produced with MidJourney version 4.
-      </span>{' '}
-      Visit an identical-looking{' '}
-      <a className="link" href="../versions/vanilla.html">
-        earlier version
-      </a>
-      .
+        . Images produced with MidJourney version 4.
+      </span>
     </footer>
   );
 }
